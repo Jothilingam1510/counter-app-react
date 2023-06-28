aeac61a35759bd5451d2bcd7e05faa0fcafccf15
Jothilingam1510 <136618756+Jothilingam1510@users.noreply.github.com>
2023-06-28 12:38:35 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
+  const decnum = () =>{
+    if(count >0){
+    setCount(count -1);
+  }else{
+    setCount(0)
+  }
+  };
   return (
     <div className="container my-5">
       <div className="card text-center my-5">
@@ -9,8 +16,8 @@ function App() {
           <h1>counter app</h1>
           <div className="my-5">
             <h2 className="my-5">{count}</h2>
-            <button className="btn btn-success mx-3" onClick={() => setCount(count + 1)}>Increament</button>
-            <button className="btn btn-danger mx-3" onClick={() => setCount(count - 1)}>Decreament</button>
+            <button className="btn btn-success mx-3"onClick={() => setCount(count + 1)}>Increament</button>
+            <button className="btn btn-danger mx-3" onClick={decnum}>Decreament</button>
             <button className="btn btn-secondary mx-3" onClick={() => setCount(0)}>Reset</button>
           </div>
 
