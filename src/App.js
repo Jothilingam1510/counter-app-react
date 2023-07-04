09a4bcca845a516ec9290dad9640b1875e462f54
Jothilingam1510 <136618756+Jothilingam1510@users.noreply.github.com>
2023-07-04 19:12:55 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+function CounterApp() {
+  const [count, setCount] = useState(0);
 
-const handleCounter = (isIncrement) => {
-    if (isIncrement) {
+const handleCounter = (action) => {
+    if (action === 'increment') {
       setCount(count + 1);
     } else {
       setCount(count > 0 ? count - 1 : 0);
@@ -10,12 +12,17 @@ const handleCounter = (isIncrement) => {
 
   return (
     <div>
-      <h1>Counter App</h1>
-      <p>Count: {count}</p>
-      <button onClick={incrementCount}>Increment</button>
-      <button onClick={decrementCount}>Decrement</button>
+      <div className="container my-5">
+      <div className="card text-center my-5">
+      <div className="card-body">
+      <h1>Counter: {count}</h1>     
+      <button className="btn btn-success mx-3" onClick={() => handleCounter('increment')}>Increment</button>
+      <button className="btn btn-danger mx-3" onClick={() => handleCounter('decrement')}>Decrement</button>
+      </div> 
+      </div> 
+      </div>  
     </div>
   );
-
+}
 
 export default CounterApp;
